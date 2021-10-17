ef2152263abcfe602465430ea053117bcd3c65f4
alihasanshaikh <alihasanshaikh2991@gmail.com>
2021-10-17 20:49:33 +0530

    js

---
+++ home.js
@@ -0,0 +1,10 @@
+//console.log('hello');
+//alert('warning , ur learning JS');
+// console.log(b)
+var JsAge = prompt('what is your age');
+
+
+// document.getElementById("fromIndex").innerHTML="I am from Home.js";
+
+
+document.getElementById("fromIndex").innerHTML=JsAge;
