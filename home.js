ff23056c0201196b736eb50ecedab207abe3f392
alihasanshaikh <alihasanshaikh2991@gmail.com>
2021-10-23 20:43:02 +0530

    stage1

--- home.js
+++ home.js
@@ -1,10 +1,21 @@
 //console.log('hello');
 //alert('warning , ur learning JS');
 // console.log(b)
-var JsAge = prompt('what is your age');
-
-
+//var JsAge = prompt('what is your age');
 // document.getElementById("fromIndex").innerHTML="I am from Home.js";
+//document.getElementById("fromIndex").innerHTML=JsAge;
 
+prompt("what is your name");
+var Score = Math.random()* 100;
+Score = Math.floor(Score)+1;
+// alert("Your Intellingence" + Score + "%");
 
-document.getElementById("fromIndex").innerHTML=JsAge;
+if (Score > 70){
+    alert("You are Enstein !" + Score + "%");
+}
+if(Score > 30 && Score <=70) {
+    alert("You are Elon Musk !" + Score + "%" )
+}
+
+if(Score <+30) {
+    alert("You are Mr Bean !" + Score + "%" )}
